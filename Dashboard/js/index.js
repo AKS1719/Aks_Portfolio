c3759e255857664a6be3d210bcbede04ad0805ae
DeployingSites <kumarsinhaakshat19@gmail.com>
2024-06-05 11:30:46 +0530

    commit

--- Dashboard/js/index.js
+++ Dashboard/js/index.js
@@ -1,3 +1,34 @@
+window.addEventListener('mousemove',(event)=>{
+    document.querySelector('.follow-cursor').style.display="block";
+    gsap.to('.follow-cursor',{
+        x:event.x,
+        y:event.y
+    })
+})
+
+gsap.from('#About img',{
+    x:-100,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger :{
+        trigger: "#About img",
+        scroller: ".my-container",
+        // markers:true,
+        start: "top 50%"
+    }
+})
+gsap.from('#About .details div',{
+    x:100,
+    duration:1.5,
+    opacity:0,
+    scrollTrigger :{
+        trigger: "#About .details div",
+        scroller: ".my-container",
+        // markers:true,
+        start: "top 50%"
+    }
+})
+
 var nav_element = document.getElementById('my-navbar');
 // console.log(nav_element);
 function showNav(){
